e3af6e16f85dda2d5a6f8cbf3575f80f20694420
Saanavi Goyal <saanavi.goyal@macaulay.cuny.edu>
2026-05-06 00:10:53 -0400

    update cafe profile works

--- backend/routes/cafes.py
+++ backend/routes/cafes.py
@@ -130,13 +130,13 @@ def register_cafe():
         hours = data.get("hours", {})
 
         DAY_MAP = {
-            "Monday": 0,
-            "Tuesday": 1,
-            "Wednesday": 2,
-            "Thursday": 3,
-            "Friday": 4,
-            "Saturday": 5,
-            "Sunday": 6,
+            "Sunday": 0,
+            "Monday": 1,
+            "Tuesday": 2,
+            "Wednesday": 3,
+            "Thursday": 4,
+            "Friday": 5,
+            "Saturday": 6,
         }
 
         hour_rows = []
